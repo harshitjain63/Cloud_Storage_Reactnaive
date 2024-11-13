043f53e5c2548d9ccf2454b2b5e7ac610bf89b23
harshit jain <harshit.33569@tmu.ac.in>
2024-11-13 21:54:00 +0530

    feat: implemented azure , aws and wasabi

--- screens/AzureFileManager.jsx
+++ screens/AzureFileManager.jsx
@@ -1,12 +1,239 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {BlobServiceClient} from '@azure/storage-blob';
+import * as DocumentPicker from 'react-native-document-picker';
+import 'core-js/features/symbol/async-iterator';
 
 const AzureFileManager = () => {
+  const [containers, setContainers] = useState([]);
+  const [selectedContainer, setSelectedContainer] = useState(null);
+  const [selectedFile, setSelectedFile] = useState([]);
+  const [containerNameInput, setContainerNameInput] = useState('');
+  const [folder, setFolder] = useState('');
+  const blobSasUrl =
+    'https://aarjavjain.blob.core.windows.net/?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2024-11-13T20:20:00Z&st=2024-11-13T12:20:00Z&spr=https&sig=AAXeIeZ4ruK272joRh0omXrLE7Z%2Bb0hCCIdoPxH0RDU%3D';
+  const blobServiceClient = new BlobServiceClient(blobSasUrl);
+
+  // Append current time to make container name unique
+  const containerName = containerNameInput + new Date().getTime();
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  console.log(selectedContainer);
+  const createContainer = async () => {
+    try {
+      console.log(`Creating container "${containerName}"...`);
+      await containerClient.create();
+      console.log(`Done. URL: ${containerClient.url}`);
+      listContainers(); // Refresh the container list
+    } catch (error) {
+      console.log('Error creating container:', error.message);
+    }
+  };
+
+  const deleteContainer = async () => {
+    const newContainerClient =
+      blobServiceClient.getContainerClient(selectedContainer);
+    try {
+      console.log(`Deleting container "${containerName}"...`);
+      await newContainerClient.delete();
+      Alert.alert('Container deleted.');
+      listContainers(); // Refresh the container list
+    } catch (error) {
+      console.log('Error deleting container:', error.message);
+    }
+  };
+
+  const selectFiles = async () => {
+    try {
+      const files = await DocumentPicker.pick({
+        type: [DocumentPicker.types.allFiles],
+        allowMultiSelection: true,
+      });
+
+      console.log('Selected files:', files);
+      if (files) {
+        uploadFiles(files, selectedContainer);
+      }
+    } catch (error) {
+      if (DocumentPicker.isCancel(error)) {
+        console.log('User cancelled document picker');
+      } else {
+        console.error('Document picker error:', error);
+      }
+    }
+  };
+
+  const listContainers = async () => {
+    try {
+      console.log('Retrieving container list...');
+      const iter = blobServiceClient.listContainers();
+      let containerList = [];
+
+      for await (const container of iter) {
+        containerList.push(container.name);
+      }
+
+      setContainers(containerList);
+      console.log('Containers:', containerList);
+    } catch (error) {
+      console.log('Error retrieving containers:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    listContainers();
+  }, []);
+
+  const listFiles = async name => {
+    const newContainerClient = blobServiceClient.getContainerClient(name);
+    setSelectedContainer(name);
+    try {
+      console.log('Retrieving file list...');
+      const iter = newContainerClient.listBlobsFlat();
+      let files = [];
+
+      for await (const blobItem of iter) {
+        files.push(blobItem.name);
+      }
+
+      if (files.length > 0) {
+        console.log('Files:', files);
+        setSelectedFile(files);
+      } else {
+        console.log('The container does not contain any files.');
+      }
+    } catch (error) {
+      console.log('Error retrieving files:', error.message);
+    }
+  };
+
+  const uploadFiles = async (files, containerName) => {
+    try {
+      console.log(`Uploading files to container "${containerName}"...`);
+
+      // Create a container client for the specified container
+      const containerClient =
+        blobServiceClient.getContainerClient(containerName);
+
+      // Iterate over the files and create promises for each upload
+      const promises = files.map(file => {
+        const blockBlobClient = containerClient.getBlockBlobClient(file.name);
+        return blockBlobClient.uploadData(file, {
+          blobHTTPHeaders: {blobContentType: file.type}, // Set content type if needed
+        });
+      });
+
+      await Promise.all(promises);
+      console.log('Upload completed.');
+      // Optionally refresh the file list for the container
+      listFiles(containerName);
+    } catch (error) {
+      console.log('Error uploading files:', error.message);
+    }
+  };
+  const createFolder = async (containerNamed, folderName) => {
+    try {
+      const folderContainerClient =
+        blobServiceClient.getContainerClient(containerNamed);
+      const emptyBlobClient = folderContainerClient.getBlockBlobClient(
+        `${folderName}/`,
+      );
+
+      // Upload an empty blob to create a "folder"
+      await emptyBlobClient.uploadData(new Blob([]));
+      console.log(
+        `Empty folder "${folderName}" created in container "${containerNamed}"`,
+      );
+    } catch (error) {
+      console.log('Error creating folder:', error.message);
+    }
+  };
+
   return (
-    <View>
-      <Text>AzureFileManager</Text>
+    <View style={{padding: 10}}>
+      <Text style={styles.txt}>
+        Currently Selected Container is : {selectedContainer}
+      </Text>
+      <Text style={styles.txt}>Azure File Manager</Text>
+      <TextInput
+        style={styles.input}
+        value={containerNameInput}
+        onChangeText={text => setContainerNameInput(text)}
+        placeholder="Enter Container name"
+        placeholderTextColor="grey"
+      />
+      <TextInput
+        style={styles.input}
+        value={folder}
+        onChangeText={text => setFolder(text)}
+        placeholder="Enter folder name"
+        placeholderTextColor="grey"
+      />
+      <Button title="Create Container" onPress={createContainer} />
+      <Button title="Delete Container" onPress={deleteContainer} />
+      <Button title="Upload Files" onPress={selectFiles} />
+      <Button
+        title="create folder"
+        onPress={() => createFolder(selectedContainer, folder)}
+      />
+      <Text style={styles.txt}>Containers:</Text>
+      <ScrollView>
+        {selectedContainer ? (
+          <View>
+            <Text style={styles.txt}>{`📂 ${selectedContainer}`}</Text>
+            {selectedFile.map((file, index) => (
+              <View key={index}>
+                <Text style={styles.txt}>
+                  {file.endsWith('/')
+                    ? `📂 ${file.slice(0, -1)}`
+                    : `📃 ${file}`}
+                </Text>
+              </View>
+            ))}
+            <Button title="Back" onPress={() => setSelectedContainer(null)} />
+          </View>
+        ) : containers.length > 0 ? (
+          containers.map((item, index) => (
+            <View key={index}>
+              <Text
+                style={styles.txt}
+                onPress={() => {
+                  listFiles(item);
+                  setSelectedContainer(item);
+                }}>
+                {`📂 ${item}`}
+              </Text>
+            </View>
+          ))
+        ) : (
+          <Text style={styles.txt}>No containers present</Text>
+        )}
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 8,
+    borderRadius: 10,
+    color: 'black',
+  },
+  txt: {
+    color: 'black',
+    marginVertical: 5,
+  },
+});
 
 export default AzureFileManager;
--- screens/FileManagerScreen.jsx
+++ screens/FileManagerScreen.jsx
@@ -1,79 +1,106 @@
-import 'react-native-url-polyfill/auto';
-import 'react-native-get-random-values';
-import 'stream-browserify';
-import 'readable-stream';
-import {Buffer} from 'buffer';
-
-global.Buffer = global.Buffer || Buffer;
-
 import React, {useEffect, useState} from 'react';
-import {View, Text, Button, Alert, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  Alert,
+  StyleSheet,
+  FlatList,
+  TextInput,
+  ActivityIndicator,
+  Modal,
+  TouchableOpacity,
+} from 'react-native';
 import {loadConfiguration} from '../utils/storage';
-import {S3Client, PutObjectCommand} from '@aws-sdk/client-s3';
 import axios from 'axios';
+import * as DocumentPicker from 'react-native-document-picker';
+import {useNavigation} from '@react-navigation/native';
 
 const FileManagerScreen = () => {
   const [configured, setConfigured] = useState(false);
-  const [client, setClient] = useState(null);
   const [serviceName] = useState('Wasabi');
+  const [file, setFiles] = useState([]);
+  const [folder, setFolders] = useState([]);
+  const [currentPath, setCurrentPath] = useState('');
+  const [folderName, setFolderName] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
+  const navigation = useNavigation();
+
+  const handleOptionSelect = screen => {
+    setModalVisible(false);
+    navigation.navigate(screen);
+  };
 
   useEffect(() => {
     loadConfiguration(serviceName).then(config => {
       if (config) {
         setConfigured(true);
-        const configuredClient = configureWasabi(
-          config.accessKey,
-          config.secretKey,
-        );
-        setClient(configuredClient);
       } else {
         Alert.alert('Error', 'Please configure AWS S3 first');
       }
     });
   }, [serviceName]);
 
+  useEffect(() => {
+    fetchFilesFolders();
+  }, [currentPath]);
+
+  const navigateTo = foldername => {
+    setCurrentPath(prevPath => prevPath + foldername);
+  };
+
   const fetchFilesFolders = async () => {
-    const response = await axios.post(
-      'https://filestoreserver.onrender.com/aws/fetch-content',
-      {
-        currentPath: '/',
-        accessKeyId: '',
-        secretAccessKey: '',
-        region: 'ap-south-1',
-        bucketName: 'aarjavharshit',
-      },
-    );
-    console.log('response', response.data);
-  };
-
-  const configureWasabi = (accessKey, secretKey) => {
-    const configuredClient = new S3Client({
-      credentials: {
-        accessKeyId: accessKey,
-        secretAccessKey: secretKey,
-      },
-      endpoint: 'https://s3.wasabisys.com',
-      region: 'us-east-1',
-    });
-    return configuredClient;
-  };
-
-  const createFolder = async folderName => {
-    if (!client) {
-      Alert.alert('Error', 'Wasabi client is not configured.');
-      return;
+    const config = await loadConfiguration(serviceName);
+    setIsLoading(true);
+    if (config) {
+      const {accessKey, secretKey} = config;
+      try {
+        const response = await axios.post(
+          'https://filestoreserver.onrender.com/wasabi/fetch-content',
+          {
+            currentPath: currentPath,
+            accessKeyId: accessKey,
+            secretAccessKey: secretKey,
+            region: 'us-east-1',
+            bucketName: 'filemanager',
+          },
+        );
+        if (response.data) {
+          setIsLoading(false);
+        }
+        console.log('Response:', response.data);
+        const {files, folders} = response.data;
+        setFiles(files);
+        setFolders(folders);
+      } catch (error) {
+        console.error('Error fetching files/folders:', error);
+        Alert.alert('Error', 'Failed to fetch files/folders');
+      }
+    } else {
+      Alert.alert('Error', 'AWS S3 configuration not found');
     }
+  };
+
+  const createFolder = async () => {
+    const config = await loadConfiguration(serviceName);
+    const {accessKey, secretKey} = config;
 
     try {
-      const result = await client.send(
-        new PutObjectCommand({
-          Bucket: 'filemanager',
-          Key: `${folderName}/`,
-          Body: 'hello',
-        }),
+      const response = await axios.post(
+        'https://filestoreserver.onrender.com/wasabi/create-folder',
+        {
+          folderName: folderName,
+          currentPath: currentPath,
+          accessKeyId: accessKey,
+          secretAccessKey: secretKey,
+          region: 'us-east-1',
+          bucketName: 'filemanager',
+        },
       );
-      Alert.alert('Success', `Folder '${folderName}' created successfully.`);
-      console.log(result);
+
+      console.log('Response:', response.data);
+      fetchFilesFolders();
     } catch (error) {
       console.error('Error creating folder:', error);
       const errorMessage = error.message || 'An unknown error occurred';
@@ -81,18 +108,147 @@ const FileManagerScreen = () => {
     }
   };
 
-  if (!configured) {
-    return <Text>Loading configuration...</Text>;
-  }
+  const goBack = () => {
+    if (currentPath === '/') {
+      setCurrentPath('');
+    }
+    setCurrentPath(prevPath => {
+      const part = prevPath.split('/').filter(Boolean);
+      part.pop();
+      return part.length > 0 ? part.join('/') + '/' : '';
+    });
+  };
 
+  const uploadFiles = async files => {
+    const config = await loadConfiguration(serviceName);
+    const {accessKey, secretKey} = config;
+
+    try {
+      const formData = new FormData();
+      formData.append('accessKeyId', accessKey);
+      formData.append('secretAccessKey', secretKey);
+      formData.append('region', 'us-east-1');
+      formData.append('bucketName', 'filemanager');
+      formData.append('currentPath', currentPath);
+
+      files.forEach(filess => {
+        formData.append('files', {
+          uri: filess.uri,
+          type: filess.type,
+          name: filess.name,
+        });
+      });
+
+      console.log('formdata', formData);
+
+      const response = await axios.post(
+        'https://filestoreserver.onrender.com/wasabi/upload-file',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        },
+      );
+
+      console.log('Upload response:', response.data);
+    } catch (error) {
+      console.error('Upload error:', error);
+    }
+  };
+
+  const selectFiles = async () => {
+    try {
+      const files = await DocumentPicker.pick({
+        type: [DocumentPicker.types.allFiles],
+        allowMultiSelection: true,
+      });
+
+      console.log('Selected files:', files);
+      if (files) {
+        uploadFiles(files);
+      }
+    } catch (error) {
+      if (DocumentPicker.isCancel(error)) {
+        console.log('User cancelled document picker');
+      } else {
+        console.error('Document picker error:', error);
+      }
+    }
+  };
+
+  if (!configured) {
+    return (
+      <Text style={styles.txt}>
+        Please do the configuration of {serviceName}
+      </Text>
+    );
+  }
+  console.log('currentpath', currentPath);
   return (
     <View style={styles.container}>
-      <Text>File Manager</Text>
-      <Button
-        title="Create Folder"
-        onPress={() => createFolder('new-folder8')}
+      <TouchableOpacity
+        onPress={() => setModalVisible(true)}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Open Menu</Text>
+      </TouchableOpacity>
+      <Modal
+        transparent={true}
+        visible={modalVisible}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            <TouchableOpacity
+              onPress={() => handleOptionSelect('AwsFileManager')}
+              style={styles.option}>
+              <Text style={styles.optionText}>Go to Aws FileManager</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleOptionSelect('AzureFileManager')}
+              style={styles.option}>
+              <Text style={styles.optionText}>Go to Azure FileManager</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setModalVisible(false)}
+              style={styles.option}>
+              <Text style={styles.optionText}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      <TextInput
+        style={styles.input}
+        value={folderName}
+        onChangeText={text => setFolderName(text)}
+        placeholder="Enter Folder name"
+        placeholderTextColor={'grey'}
       />
-      <Button title="Get Data" onPress={() => fetchFilesFolders()} />
+      <Button title="Create Folder" onPress={() => createFolder()} />
+      <Button title="Upload Files" onPress={selectFiles} />
+      <Text style={styles.sectionTitle}>Content:</Text>
+      {isLoading && <ActivityIndicator />}
+      <FlatList
+        data={folder.concat(file)}
+        keyExtractor={(item, index) => item.name + index}
+        renderItem={({item}) => (
+          <View style={styles.item}>
+            <Text
+              onPress={() =>
+                item.name.endsWith('/')
+                  ? navigateTo(item.name)
+                  : Alert.alert(item.name)
+              }
+              style={styles.txt}>
+              {item.name.endsWith('/')
+                ? `📂 ${item.name.slice(0, -1)}`
+                : `📃 ${item.name}`}
+            </Text>
+          </View>
+        )}
+      />
+      {currentPath && <Button title="Go back" onPress={goBack} />}
     </View>
   );
 };
@@ -100,6 +256,64 @@ const FileManagerScreen = () => {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 20,
+    color: 'black',
+  },
+  item: {
+    padding: 10,
+    marginVertical: 5,
+    backgroundColor: '#f1f1f1',
+    borderRadius: 5,
+  },
+  txt: {
+    color: 'black',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 8,
+    borderRadius: 10,
+    color: 'black',
+  },
+  button: {
+    padding: 15,
+    backgroundColor: '#6200ea',
+    borderRadius: 5,
+    margin: '3%',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    width: 300,
+    alignItems: 'center',
+  },
+  option: {
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    width: '100%',
+    alignItems: 'center',
+  },
+  optionText: {
+    fontSize: 18,
+    color: 'black',
   },
 });
 
